c710e0f4bdbb9a8310d44614b18e6e02d5bc0f92
takahashim <takahashimm@gmail.com>
2024-02-23 00:06:47 +0900

    Use DenoKV

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,41 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+/// <reference lib="deno.unstable" />
 
-export default function Home() {
-  const count = useSignal(3);
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+interface Data {
+  comments: string[];
+}
+
+const setComment = async (title: string, content: string) => {
+  const kv = await Deno.openKv();
+  await kv.set(["comments", title], content);
+}
+
+const getComments = async () => {
+  const kv = await Deno.openKv();
+  const iter = await kv.list<string>({prefix: ["comments"]});
+  const comments = [];
+  for await (const entry of iter) {
+    comments.push(`${entry.key}: ${entry.value}`); 
+  }
+  return comments;
+}
+
+export const handler: Handlers<Data> = {
+  async GET(req, ctx) {
+    const url = new URL(req.url);
+    const title = url.searchParams.get("title") || "-";
+    const content = url.searchParams.get("content") || "---";
+    await setComment(title, content);
+    const comments = await getComments();
+
+    return ctx.render({ comments });
+  }
+};
+
+export default function Home({ data }: PageProps<Data>) {
+  const { comments } = data;
+
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -18,7 +51,16 @@ export default function Home() {
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
         </p>
-        <Counter count={count} />
+      </div>
+      <div>
+        <form>
+          <label>title: <input type="text" name="title"  /></label>
+          <label>content:<input type="text" name="content"  /></label>
+          <button type="submit">Submit</button>
+        </form>
+        <ul>
+          {comments.map((comment) => <li>{comment}</li>)}
+        </ul>
       </div>
     </div>
   );
